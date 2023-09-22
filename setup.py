2b4eda1f88e614b0bc3167df4a0cf228ddd25a07
Antonio Almudevar Atienza <almudevar@hermes2.i3a.es>
2023-09-22 13:34:12 +0200

    setup.py added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(  name='variational_translation',
+        version='1.0',
+        description='Variational translation of change of domain',
+        author='Antonio Almudevar',
+        author_email='almudevar@unizar.es',
+        packages=['src'],
+    )
