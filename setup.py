b616e0d68bbf72221f2d89f8d8c8c77aa682d330
Antonio Almudevar Atienza <almudevar@hermes2.i3a.es>
2023-09-22 13:41:11 +0200

    Upated README.md

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(  name='variational_translation',
         version='1.0',
-        description='Variational translation of change of domain',
+        description='Unsupervised Multiple Domain Translation through Controlled Disentaglement in Variational Autoencoder',
         author='Antonio Almudevar',
         author_email='almudevar@unizar.es',
         packages=['src'],
